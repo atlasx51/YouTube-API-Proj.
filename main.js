22ebee5e665eaa390af9b6fe61746ef013c42f1f
Eric Vargas <MacHD@erics-imac.attlocal.net>
2018-07-31 22:41:11 -0700

    2nd commit

--- main.js
+++ main.js
@@ -14,3 +14,71 @@ const content = document.getElementById('content');
 const channelForm = document.getElementById('channel-form');
 const channelInput = document.getElementById('channel-input');
 const videoContainer = document.getElementById('video-container');
+
+const defaultChannel= 'techguyweb';
+
+// load auth2 library
+function handleClientLoad() {
+    gapi.load('client:auth2, initClient');
+
+}
+
+//Init API client library and set up sign in listeners
+function initClient(){
+    gapi.client.init({
+        discoveryDocs: DISCOVERY_DOCS,
+        clientId: CLIENT_ID,
+        scope: SCOPES
+    }).then(() => {
+        //listen for sign in state changes
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+        //handle initial sign in state
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    });
+}
+
+//update ui sign in state change
+
+
+function updateSigninStatus(isSignedIn) {
+    if(isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        content.style.display = 'block';
+        videoContainer.style.display = 'block';
+        getChannel(defaultChannel);
+        
+    }else{
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+        content.style.display = 'none';
+        videoContainer.style.display = 'none';
+    }
+}
+
+///handle login
+function handleAuthClick(){
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+//handle logout
+
+function handlSignClick(){
+    gapi.auth2.getAuthInstance().signOut(); 
+}
+
+//get channel from api
+
+function getChannel(channel){
+    gapi.client.youtube.channels.list({
+        part: 'snippet,contentDetails,statistics',
+        forUsername: channel
+
+    })
+    .then(respone => {
+        console.log(response);;
+    })
+    .catch(err => alert('No Channel By That Name'));
+}
